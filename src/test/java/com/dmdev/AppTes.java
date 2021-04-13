02124d9e8c234cd7e9a692c38b0606ebbacc8a22
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-04-13 09:08:50 +0300

    added files from lesson-13

---
+++ src/test/java/com/dmdev/AppTes.java
@@ -0,0 +1,19 @@
+package com.dmdev;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTes {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue3() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/com/dmdev/AppTestCase.java
@@ -0,0 +1,19 @@
+package com.dmdev;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTestCase {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue2() {
+        assertTrue(true);
+    }
+}
