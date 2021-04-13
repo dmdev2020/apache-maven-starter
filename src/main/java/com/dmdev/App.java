e61ab36306783bc921a97d4bf28d5bffc76cc819
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-04-13 08:48:33 +0300

    added files from lesson-12

--- src/main/java/com/dmdev/App.java
+++ src/main/java/com/dmdev/App.java
@@ -1,11 +1,14 @@
 package com.dmdev;
 
+import java.util.function.Supplier;
+
 /**
  * Hello world!
  */
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Supplier<String> s = () -> "Hello World";
+        System.out.println(s.get());
     }
 }
