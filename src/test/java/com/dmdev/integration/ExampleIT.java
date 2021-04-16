7b55117cac43de2ef211bbbd58b43ea17de4104a
Dzianis Matveyenka <dmatveyenka@fitbit.com>
2021-04-16 10:28:11 +0300

    added files from lesson-17

---
+++ src/test/java/com/dmdev/integration/ExampleIT.java
@@ -0,0 +1,13 @@
+package com.dmdev.integration;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ExampleIT {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
